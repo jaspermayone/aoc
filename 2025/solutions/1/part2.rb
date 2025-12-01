b3c970378ca8336f81c06bb9ab51ecdb248afeb1
Jasper Mayone <me@jaspermayone.com>
2025-12-01 08:42:20 -0500

    2025: day 1

---
+++ 2025/solutions/1/part2.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+def main(argv)
+  options = {}
+  OptionParser.new do |opts|
+    opts.banner = 'Usage: part1.rb [options]'
+    opts.on('-fFILE', '--file=FILE', 'File to scan') { |v| options[:file] = v }
+  end.parse!(argv)
+
+  file = options[:file]
+
+  pos = 50
+  zeros = 0
+
+  File.foreach(file) do |line|
+    direction = line[0]
+    distance  = line[1..].strip.to_i
+
+    case direction
+    when 'L'
+      # pos = (pos - distance) % 100
+
+      (1..distance).each do |i|
+        pos = (pos - 1) % 100
+        if pos == 0
+          zeros += 1
+        end
+      end
+
+    when 'R'
+      # pos = (pos + distance) % 100
+
+      (1..distance).each do |i|
+        pos = (pos + 1) % 100
+        if pos == 0
+          zeros += 1
+        end
+      end
+
+    end
+
+  end
+
+  puts "zeros #{zeros}"
+
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  main(ARGV)
+end
