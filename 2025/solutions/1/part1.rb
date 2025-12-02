2df62982b65b18300b2567c5194e9f5e845af666
Jasper Mayone <me@jaspermayone.com>
2025-12-01 23:10:55 -0500

    2025: quick cleanup on pt1 of day 1

--- 2025/solutions/1/part1.rb
+++ 2025/solutions/1/part1.rb
@@ -5,20 +5,18 @@ zeros = 0
 
 File.foreach('input.txt') do |line|
 
-    direction = line[0]
-    distance  = line[1..].strip.to_i
+  direction = line[0]
+  distance  = line[1..].strip.to_i
 
-    case direction
-    when 'L'
-      pos = (pos - distance) % 100
+  case direction
+  when 'L'
+    pos = (pos - distance) % 100
 
-    when 'R'
-      pos = (pos + distance) % 100
-    end
+  when 'R'
+    pos = (pos + distance) % 100
+  end
 
-    if pos == 0
-      zeros += 1
-    end
+  zeros += 1 if pos.zero?
 end
 
 puts "zeros #{zeros}"
